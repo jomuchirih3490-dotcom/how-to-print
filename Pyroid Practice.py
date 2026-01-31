8545974274ef722fdd4ee6a5fe1a5b24b7957fd4
John <jomuchirih3490@gmail.com>
2026-01-31 14:10:55 +0300

    Add files via upload

---
+++ Pyroid Practice.py
@@ -0,0 +1,38 @@
+print("john")
+print("lessons fo AI and Data science in the yeqr 2026") 
+print("lessons fo AI and Data science","in the yeat 2026")
+print("kuwait ","mauritious","usa","kenya","Dubai")  
+print("Kuwait"+"Mauritious"+"USA")
+x="a+b" 
+print(x)
+print ("x")
+X=2+1
+print(X)
+print(type(x))
+Y=47
+Y2="47" 
+print (type(Y))
+print(type(Y2))
+Y2=int(Y2)
+print(type(Y2))
+
+Z=4.4
+print(type(Z))
+Dayshift=True
+print(type(Dayshift))
+print(x,Y,Y2,Z,True) 
+
+Username=input("Enter your name:")
+
+print("Welcome", Username)
+
+D=input("when were you born:")
+D=int(D)
+ 
+Age=2026-D
+print ("you are age",Age,"yrs old")
+ 
+print(f"you are age {Age}yrs old")
+print(f"welcome {Username} you are age {Age}yrs old")
+ 
+ 
